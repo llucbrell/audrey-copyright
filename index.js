c35d6835e17fa4af560c9007dda1abd50c86c7e3
Lucas C/llucbrell <llucbrell@gmail.com>
2015-09-18 14:35:41 +0200

    copyright v.1.0

---
+++ index.js
@@ -0,0 +1,61 @@
+
+module.exports= function(){
+
+var data;
+var name;	
+var bul;
+var terminal;
+
+
+
+return{
+	     grow: function(simName, customObject){ 
+        terminal= customObject;
+        printCopyright(simName);},
+harvestData: function(){ return data;}
+}
+
+function checkColors(name){
+  var colors=Object.getOwnPropertyNames(terminal.colors);
+  bul=false; // boolean to control if its defined the property
+
+  for(var i=0; i<colors.length; i++){//iterate over prop names
+
+    if(colors[i] === name){//if it is
+      bul=true;
+    }
+  }
+  if(bul!==true){//if its finded the statement of the tag
+    terminal.colors[name]=terminal.colors.default;
+    } 
+}
+
+
+function checkProperties(name){
+var properties= Object.getOwnPropertyNames(terminal);
+  bul=false; // boolean to control if its defined the property
+
+  for(var i=0; i<properties.length; i++){//iterate over prop names
+    if(properties[i] === name){//if it is
+      bul=true;
+    }
+  }
+  if(bul!==true){//if its finded the statement of the tag
+    throw new Error('Not defined '+name);
+    }  
+}
+
+function printCopyright(simName){ 
+var name= simName.slice(2); //delete simbol of the name 
+  checkProperties(name);
+  checkColors(name);
+    var copyoutput=terminal[name].split(" ");//get the words
+    console.log(); 
+    console.log(terminal.colors[name](copyoutput[0] +" \xA9 "+copyoutput.slice(1).join(" ")));
+    console.log();//output the license name + symbol + name
+}
+
+
+
+
+}
---
+++ readme.md
@@ -0,0 +1,41 @@
+# audrey-copyright-&&
+
+> Seed for audrey-two VCCLI (View-Control-Command-Line-Interface) ^v2.0.0
+
+
+## What this seed does?
+
+Include a copyright on your audrey-view.
+
+## Specifications
+
+Audrey-two version 2.0.0 change the way of control your CLI-views. The tags, now replaced by custom audrey-seeds that you can include or not in your projects.
+
+This node-module redefine the older audrey-two taggy "&". Now you can use it again with audrey.
+
+## Install
+
+Example
+```
+$ npm install --save audrey-two
+  npm install --save audrey-copyright-&&
+```
+## Usage
+
+Example
+
+```js
+var myTerminalDisplay={
+	footer:["&&myCopy"],//tell audrey where you want to display it
+	myCopy:"MIT Lucas_C/llucbrell",//tell audrey what to display
+	colors:{myCopy:'red'}//tell audrey wich color to use
+};
+
+var audrey2= require('audrey-two');
+var audrey= audrey2(myTerminalDisplay);
+
+//run your command and pass error objects to audrey
+
+audrey.seed(["audrey-copyright-&&"]);//pass audrey the name of a new seed
+ audrey.talk();//run audrey to display view
+```
